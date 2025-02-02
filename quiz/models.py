636d3db8f393fdd0367772c16e6d94d15ab81b44
= <=>
2025-02-02 20:48:13 +0300

    add UserProfile model

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to="", null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
